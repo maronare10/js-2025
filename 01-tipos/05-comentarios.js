3c84b3c81b962cdb3a2ba9fa6f97e171d7d725e3
Mariana Navarro <maronare10@gmail.com>
2025-08-29 15:54:18 -0500

    objetos en js

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,7 @@
+//Este es un comentario de línea
+
+/* Este es un comentario de muchas líneas (de párrafo), los comentarios no son tomados en cuenta por js */
+
+let numero = 42; //Este es otro comentario de línea
+
+console.log(numero);
