34c1b7a9e7b8592b5335b167e85c9b3e92c4ce80
Mariana Navarro <maronare10@gmail.com>
2025-09-02 00:23:58 -0500

    los arrays

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,21 @@
+let animales =  ['chanchito', 'caballo'] //array literal se usa parentesis cuadrados (corchetes)
+
+console.log(animales);
+
+console.log(animales[0]);
+
+animales[2] = 'dragon'; //agregando elementos
+console.log(animales); 
+
+animales[10] ='pez';
+console.log(animales[7]);
+console.log(typeof animales);
+
+console.log(animales.length) 
+
+
+
+
+
+
+
