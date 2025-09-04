fbca96d13bd94df2b670302ef3f35eb5589e84d0
Mariana Navarro <maronare10@gmail.com>
2025-09-03 23:35:37 -0500

    operadores de aignación

---
+++ 02-operadores/02-asignacion.js
@@ -0,0 +1,13 @@
+let a = 5;
+// let  a = a + 5;
+a = a + 5;
+
+//Con ++ o -- incremento o decremento de 1 en 1, pero cuando quiero incrementar o decrementar otra cantidad, lo hago de esta manera.
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+
+console.log(a);
