5306c82a100be071a42cd5b93b7fdc3f82905e87
Mariana Navarro <maronare10@gmail.com>
2025-09-03 23:35:07 -0500

    operadores  aritmeticos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,19 @@
+let a = 5; // declaration
+let b = 7; 
+
+/* console.log(a + b, 'adición'); // adicion ; expression
+console.log(a - b, 'resta'); // resta 
+console.log(a * b, 'multiplicación'); // multiplicación 
+console.log(a / b, 'división') ; // división 
+console.log(a % b, 'modulo'); // módulo 
+console.log(a ** b, 'potencia'); // potencia  */
+
+// incrementar
+console.log(++a); //incrementa el valor en 1
+console.log(a++); //primero imprime el valor de a y después incrementa el valor en 1, para ello hay que hacer otro console.log
+console.log(a);
+
+// decrementar
+console.log(--a); //decrementa el valor en 1
+console.log(a--); //primero imprime el valor de a y después decrementa el valor en 1, para ello hay que hacer otro console.log
+console.log(a);
