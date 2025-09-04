5381ce2244a55d046e123459b9bc38fd5bd9d159
Mariana Navarro <maronare10@gmail.com>
2025-09-03 23:55:46 -0500

    operador de comparación

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+// Operadores relacionales
+let a = 10;
+console.log((a > 5));
+console.log((a >= 5));
+console.log((a < 5));
+console.log((a <= 5));
+
+// Operadores de igualdad
+console.log(a == 10);
+console.log(a != 10);
+console.log(a == '10');
+console.log(a === '10');
+console.log(a !== '10');
